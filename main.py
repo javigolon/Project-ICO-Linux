8b8f799ab1b8ae569a64c652677f8952bd47dcda
javigolon <smskaliosman@gmail.com>
2025-08-29 18:46:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import subprocess
 import texture_export
 import model_export
 
-
 if (len(sys.argv) != 2):
     print("Please provide the path to data.df")
     sys.exit()
 print(sys.argv[1])
-subprocess.call(["df-read2.exe",sys.argv[1]])
+subprocess.call("./df-read2.exe", shell=True)
 
 path = 'data'
 
